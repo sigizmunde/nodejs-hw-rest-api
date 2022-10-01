e69a50e57e0643a596e951a29f02cec324c589b1
illya bondar <1816579@gmail.com>
2022-10-01 23:26:54 +0300

    REST API first approach

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,42 @@
-const express = require('express')
+const express = require("express");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require("../../models/contacts");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_, res) => {
+  const data = await listContacts();
+  res.json(data);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res) => {
+  const id = req.params.contactId;
+  const contact = await getContactById(id);
+  res.json(contact);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res) => {
+  const body = req.query;
+  const contact = await addContact(body);
+  res.json(contact);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res) => {
+  const id = req.params.contactId;
+  const contact = await removeContact(id);
+  res.json(contact);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res) => {
+  const id = req.params.contactId;
+  const body = req.query;
+  const contact = await updateContact(id, body);
+  res.json(contact);
+});
 
-module.exports = router
+module.exports = router;
