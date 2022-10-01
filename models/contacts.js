457bcdef36967c4bb603be9b2f720b7ebbef18c3
illya bondar <1816579@gmail.com>
2022-10-01 22:55:11 +0300

    CRUD data methods added

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,61 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function loadData() {
+  const file = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(file) || [];
+}
 
-const removeContact = async (contactId) => {}
+async function saveData(data) {
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+}
 
-const addContact = async (body) => {}
+const listContacts = async () => {
+  const data = await loadData();
+  return data;
+};
 
-const updateContact = async (contactId, body) => {}
+async function getContactById(contactId) {
+  const data = await loadData();
+  const contact = data.find((el) => el.id === String(contactId));
+  return contact || null; // generate error
+}
+
+async function removeContact(contactId) {
+  const data = await loadData();
+  const index = data.findIndex(({ id }) => id === String(contactId));
+  if (index === -1) return null; // generate error
+  const [contact] = data.splice(index, 1);
+  await saveData(data);
+  return contact;
+}
+
+async function addContact(body) {
+  const { name, email, phone } = body;
+  const data = await loadData();
+  const id = uuidv4();
+  const contact = { id, name, email, phone };
+  data.push(contact);
+  await saveData(data);
+  return contact;
+}
+
+const updateContact = async (contactId, body) => {
+  const data = await loadData();
+  const index = data.findIndex((el) => el.id === String(contactId));
+  if (index === -1) {
+    console.log("No contact");
+    return null; // generate error
+  }
+  const contact = data[index];
+  const updatedContact = { ...contact, ...body };
+  data[index] = updatedContact;
+  await saveData(data);
+  return updatedContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +63,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
