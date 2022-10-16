f8848ea0caf815894a56a7d760eeffa9f8140f23
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-10-16 23:57:13 +0300

    minor fix: register response corrected

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -5,16 +5,20 @@ const { User } = require("../../models/userModel");
 const { createError } = require("../../helpers");
 
 const register = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, subscription } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw createError(409, "Email in use");
   }
   const hashPassword = await bcrypt.hash(password, 12);
-  const result = await User.create({ email, password: hashPassword });
+  const result = await User.create({
+    email,
+    password: hashPassword,
+    subscription,
+  });
   res.status(201).json({
-    name: result.name,
     email: result.email,
+    subscription: result.subscription,
   });
 };
 
